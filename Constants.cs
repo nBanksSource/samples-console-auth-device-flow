9ef19692feacf49680b5c2b4aa66fae4d9f9432e
Fernando Mazzocchi <fernando.mazzocchi@nbanks.net>
2020-09-17 16:52:29 +0100

    chore: add test-client context

--- Constants.cs
+++ Constants.cs
@@ -10,8 +10,14 @@ namespace Clients
         //SANDBOX
         public const string Authority = "https://login-sandbox.nbanks.net";
         public const string SampleApi = "https://sandbox.nbanks.net/";
+
+        // SAMPLE-CLIENT
         public const string SampleClientId = "device";
         public const string SampleClientSecret = "E39F33E9-C1BD-4D5F-895F-9D9E3982B557";
+
+        //TEST-CLIENT
+        // public const string SampleClientId = "[Your_ClientId]";
+        // public const string SampleClientSecret = "[Your_ClientSecret]";
 
     }
 }
