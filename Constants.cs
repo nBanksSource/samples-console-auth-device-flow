d44504ae02785b24313285213d4fd5b8af2f2ebf
Fernando Mazzocchi <fernando.mazzocchi@nbanks.net>
2020-11-05 14:44:15 +0000

    feat: add production uri to config

--- Constants.cs
+++ Constants.cs
@@ -3,19 +3,19 @@ namespace Clients
     public class Constants
     {
 
-        //LOCAL
-        // public const string Authority = "http://host.docker.internal:5550";
-        // public const string SampleApi = "http://localhost:5210/";
-        
-        //SANDBOX
+        // SANDBOX
         public const string Authority = "https://login-sandbox.nbanks.net";
         public const string SampleApi = "https://sandbox.nbanks.net/";
+
+        // PROD
+        // public const string Authority = "https://login.nbanks.net";
+        // public const string SampleApi = "https://api.nbanks.net/";
 
         // SAMPLE-CLIENT
         public const string SampleClientId = "device";
         public const string SampleClientSecret = "E39F33E9-C1BD-4D5F-895F-9D9E3982B557";
 
-        //TEST-CLIENT
+        // TEST-CLIENT
         // public const string SampleClientId = "[Your_ClientId]";
         // public const string SampleClientSecret = "[Your_ClientSecret]";
 
